08c92b92b5882a724e54ace7be9c8e5dea9d5805
Solst1tium <solange.leiva@mayor.cl>
2020-09-12 00:32:53 -0300

    creacion de vista productos y llamada a Api

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,29 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+<v-app>
+  <v-container grid-list-xs>
+    <v-app-bar app color="dark" dark>
+      <v-toolbar-title></v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-btn to="/" text>home</v-btn>
+      <v-btn to="/products" text>Productos</v-btn>
+      <v-spacer></v-spacer>
     </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
-</template>
+      <router-view></router-view>
+    </v-main>    
+    <v-footer
+        absolute
+        class="font-weight-medium" color="indigo lighten-4">
+        <v-col class="text-center" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-footer>
+   </v-container>
+  </v-app>  
+</template> 
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  name:"app"
+}
 </script>
---
+++ src/views/Products.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-content>
+    <v-row>
+      <v-col cols="6">
+        <v-img :src="product.image_link" height="200px"></v-img>
+      </v-col>
+    </v-row>
+ 
+     <v-row>
+      <v-col cols="6">
+        <v-card-title>{{product.name}}</v-card-title>
+      </v-col>
+    </v-row>
+  </v-content>
+</template>
+<script>
+
+export default {
+
+  
+}
+</script>
